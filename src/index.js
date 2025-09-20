6d6c344c364259aeff6f05e12f0121b405600f1c
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-09-20 12:19:35 +0300

    Init express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log(111);
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+    res.write("Test");
+    res.end();
+});
+
+app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
