29ef70c35fdc2f47ee91e488ce86d043271b389e
dibr <daniel.bratov@unicontsoft.com>
2025-10-08 15:51:11 +0300

    Add auth service for login

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,19 @@
+import bcrypt from "bcrypt"
 import User from "../models/Auth.js";
+import { GenAccessToken } from "../utils/token.utils.js";
 
-export async function Register(user){
-    return await User.create(user);
+export async function Register(userData){
+    return await User.create(userData);
+}
+
+export async function Login(email, pass){
+    const user = await User.findOne({ email });    
+    if (!user){
+        throw new Error("Invalid user or password");
+    }
+
+    const passCheck = await bcrypt.compare(pass, user.pass);
+    if (!passCheck){
+        throw new Error("Invalid user or password");
+    }
 }
